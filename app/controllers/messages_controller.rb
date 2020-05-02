72c531b600221ab1d7c272b1eccebb9888a268dd
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-05-02 15:01:41 +0000

    DRY

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,57 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only:[show, new, update, destroy]
+  
+  def index 
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+    
+    if @message.save
+      flach[:success] = "Messageが正常に投稿されました"
+      redirect_to @message
+    else
+      flash.now[:danger] = "Messageが投稿されませんでした"
+      render :new
+    end
+  end
+  
+  def new
+    @message = Message.new
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def update
+    
+    if @message.update(message_params)
+      flash[:success] ="Messageは正常に更新されました。"
+      redirect_to @message
+    else
+      flash[:danger] = "Messageは更新されませんでした。"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    
+    flash[:saccess] = "Messageは正常に削除されました。"
+    redirect_to messages_url
+  end
+
+  private
+  
+  def message_params
+    params.require(:message).permit(:content)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
+end
