3c709a415b4a7c0c805636b5d281ee9fd1b039e2
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-05-02 15:43:58 +0000

    validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only:[show, new, update, destroy]
+  before_action :set_message, only:[:show, :edit, :update, :destroy]
   
   def index 
     @messages = Message.all
